8ca6c6682b5341d9c4db434c69ca1af2501fa693
Gregory Benner <gregbenner1@gmail.com>
2013-06-20 09:23:09 -0400

    Added singleton trait

---
+++ Klathmon/Traits/Singleton.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by: Gregory Benner.
+ * Date: 6/20/13
+ */
+
+namespace Klathmon\Traits;
+
+trait Singleton
+{
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            try {
+                $reflection     = new \ReflectionClass(__CLASS__);
+                self::$instance = $reflection->newInstanceArgs(func_get_args());
+            } catch (\Exception $e) {
+                throw new \Exception('Error creating object!');
+            }
+        }
+
+        return self::$instance;
+    }
+
+    public static function killInstance()
+    {
+        self::$instance = null;
+    }
+
+    final private function __clone()
+    {
+    }
+
+    final private function __wakeup()
+    {
+    }
+}

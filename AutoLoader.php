18e10ab7af808ec626cd98f3a70e0c05b7e424b8
Gregory Benner <gregbenner1@gmail.com>
2013-06-23 11:07:07 -0400

    Added Wrench to AutoLoader

--- AutoLoader.php
+++ AutoLoader.php
@@ -22,6 +22,9 @@ function AutoLoader($class)
     if ($library == __NAMESPACE__) {
         //Any of my classes.
         $path = __DIR__ . DIRECTORY_SEPARATOR . 'Klathmon' . DIRECTORY_SEPARATOR;
+    } elseif ($library == 'Wrench') {
+        //WideImage Library.
+        $path = __DIR__ . DIRECTORY_SEPARATOR . $library . DIRECTORY_SEPARATOR;
     } elseif ($library == 'WideImage') {
         //WideImage Library.
         $path           = __DIR__ . DIRECTORY_SEPARATOR . 'WideImage' . DIRECTORY_SEPARATOR;
